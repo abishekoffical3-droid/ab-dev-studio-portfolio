5a124c0c2ee58de71d72cabf8b7705d98ced35f0
ABISHEK BHUSAL <abishekoffical3@gmail.com>
2026-07-19 16:06:51 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+==========================================
+AB Dev Studio Portfolio
+Author: Abishek Bhusal
+Version: 1.0
+==========================================
+*/
+
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("AB Dev Studio Portfolio Loaded Successfully 🚀");
+});
